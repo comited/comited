c8a779cc7f73f3b810c6b1ef8e18ca86e468590c
Lachlan Jowett <lochyjowett1@gmail.com>
2022-11-29 21:45:25 +1100

    init

---
+++ src/scripts/fetchdata.js
@@ -0,0 +1,8 @@
+// import { Octokit } from "octokit";
+
+// const octokit = new Octokit({ auth: `personal-access-token123` });
+
+// function fetchData() {
+//     const {data: { login },} = await octokit.rest.users.getAuthenticated();
+//       console.log("Hello, %s", login);
+// }
